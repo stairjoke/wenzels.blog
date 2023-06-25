ab63118b5774f12493bac88c0003a41308d941a4
Wenzel Massag <mail@wmassag.de>
2023-06-25 21:04:10 +0200

    improved date time handling in page title

--- site/snippets/default-page-title.php
+++ site/snippets/default-page-title.php
@@ -5,8 +5,23 @@
 	?>
 	<time datetime="<?= $page->date()->toDate('Y-MM-FF kk-mm-ss.SSSxxx') ?>">
 		<?php
+			// Builds an internationalized/translated statement that looks like this
+			// Published on Month DD, YYYY, HH:MM AM/PM by Wenzel Massag
+			// Or looks like
+			// Published on Month DD, YYYY by Wenzel Massag
+			
 			echo(t('publishedDate') . ' ');
-			echo($page->date()->toDate(new IntlDateFormatter($kirby->language()->locale(LC_COLLATE), IntlDateFormatter::RELATIVE_LONG, IntlDateFormatter::SHORT, 'Europe/Berlin')));
+			
+			if($page->date()->toDate('kkmmssSSS') != "240000000"){
+				// If the time is not exactly midnight
+				// Format the date and time string with time
+				echo($page->date()->toDate(new IntlDateFormatter($kirby->language()->locale(LC_COLLATE), IntlDateFormatter::RELATIVE_LONG, IntlDateFormatter::SHORT, 'Europe/Berlin')));
+			}else{
+				// If the time is exactly midnight
+				// Format the date string without time
+				echo($page->date()->toDate(new IntlDateFormatter($kirby->language()->locale(LC_COLLATE), IntlDateFormatter::RELATIVE_LONG, IntlDateFormatter::NONE, 'Europe/Berlin')));
+			}
+			
 			echo(' ' . t('publishedBy'));
 			?> <nobr>Wenzel Massag.</nobr>
 	</time>
