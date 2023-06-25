72ebbfb7245ef854f70404afcb5c7b8f5c7cc97c
Wenzel Massag <mail@wmassag.de>
2023-06-25 22:42:11 +0200

    removed unncessesary comment

--- site/snippets/default-main.php
+++ site/snippets/default-main.php
@@ -15,11 +15,6 @@
 		</div>
 		<div class="main">
 			<?php
-			/*
-				- Add notice to untranslated posts, because Kirby always auto - serves the original language if not available. Create in duplicate .de.md file with original content.
-				- Homepage will show untranslated pages, so don’t translate titles of pages that don’t get a translation
-			*/
-			
 			if(gettype($resultOfInlineNavigation) == 'array'){
 				echo kt($resultOfInlineNavigation[0]);
 			}else{
