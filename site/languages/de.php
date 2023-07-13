2082c34c85f1ebe120069274c29327ca3bd71192
Wenzel Massag <mail@wmassag.de>
2023-07-13 18:06:09 +0200

    layout updates to split home and default, added paginated content to home

--- site/languages/de.php
+++ site/languages/de.php
@@ -11,7 +11,7 @@ return [
     'translations' => [
         'publishedDate' => 'Veröffentlicht am',
         'publishedBy' => 'von',
-        'permalinkInfo' => 'Sollte sich die URL (Adresse) dieser Seite ändern, funktioniert der Permalink zu dieser Seite weiterhin. Permalinks leiten außerdem die Besucher automatisch zur passenden Übersetzung weiter.'
+        'permalinkInfo' => 'Sollte sich die URL (Adresse) dieser Seite ändern, funktioniert der Permalink zu dieser Seite weiterhin. Permalinks leiten außerdem die Besucher automatisch zur passenden Übersetzung weiter, wenn diese verfügbar ist.'
     ],
     'url' => NULL
 ];
