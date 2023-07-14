fe8bf9ddb57d9ba401c504a29d3b49ec5a891985
Wenzel Massag <mail@wmassag.de>
2023-07-14 11:31:16 +0200

    added pagination with page-links

--- site/snippets/home-main.php
+++ site/snippets/home-main.php
@@ -2,12 +2,29 @@
 <div class="page-layout home">
 	<?php
 		// Get all blogposts
-		$articles = $kirby->collection("blogposts");
+		$articles = $kirby->collection("blogposts")->paginate(10);
 		
-		foreach($articles->paginate(10) as $article) :
+		foreach($articles as $article) :
 	?>
 		<article>
 			<?= $article->title() ?>
 		</article>
 	<?php endforeach; ?>
+	
+	<?php if($articles->pagination()->hasPages()) : ?>
+	<nav class="pagination">
+		
+		<?php if($articles->pagination()->hasNextPage()) : ?>
+		<a href="<?= $articles->pagination()->nextPageURL() ?>"><?= t('paginationPreviousLink') ?></a>
+		<?php endif; ?>
+		&nbsp;|&nbsp;<!-- add link to calendar-view here -->
+		<?php if($articles->pagination()->hasPrevPage()) : ?>
+		<a href="<?= $articles->pagination()->prevPageURL() ?>">
+			<?= t('paginationNextLink') ?>
+		</a>
+		<?php endif; ?>
+		
+	</nav>
+	<?php endif; ?>
+	
 </div>
