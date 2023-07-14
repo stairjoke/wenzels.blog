64ce223488782856ee9913caa19dc3ab839fd274
Wenzel Massag <mail@wmassag.de>
2023-07-14 11:30:34 +0200

    added translation: pagination link previous next page strings

--- site/languages/en.php
+++ site/languages/en.php
@@ -11,7 +11,9 @@ return [
     'translations' => [
         'publishedDate' => 'Published on',
         'publishedBy' => 'by',
-        'permalinkInfo' => 'In case the URL (address) of this page changes, the permalink to this page will continue to function. Permalinks will also redirect visitors to the appropriate translation automatically, if it is available.'
+        'permalinkInfo' => 'In case the URL (address) of this page changes, the permalink to this page will continue to function. Permalinks will also redirect visitors to the appropriate translation automatically, if it is available.',
+        'paginationPreviousLink' => 'older posts',
+        'paginationNextLink' => 'newer posts'
     ],
     'url' => NULL
 ];
