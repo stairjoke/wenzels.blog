5e57d118cea0a10187cf9b4f8a199e39a1150a99
Wenzel Massag <mail@wmassag.de>
2023-07-20 14:43:06 +0200

    comment update

--- site/plugins/linkToPage/index.php
+++ site/plugins/linkToPage/index.php
@@ -1,5 +1,5 @@
 <?php
-	// Link to Page Controller
+	// Link to Page: ~~Controller~~ Plugin
 	
 	function linkToPage($linkedPage) {
 		return("<a href='{$linkedPage->url()}'>{$linkedPage->title()}</a>");
