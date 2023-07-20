8e9c9a9ae7f161ae5397d263aafa9c548096988a
Wenzel Massag <mail@wmassag.de>
2023-07-20 14:46:16 +0200

    added content to html title

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="<?= $kirby->language()->locale(LC_COLLATE) ?>">
 	<head>
-		<title></title>
+		<title><?= $site->title() ?> &gt; <?= $page->title() ?></title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://use.typekit.net/pqb7qdl.css">
