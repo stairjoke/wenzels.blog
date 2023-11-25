777b44ab1e222abd8d37166f08ad61a1513dc640
Wenzel Massag <mail@wmassag.de>
2023-11-25 16:28:08 +0100

    homepage shows full articles

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,33 +3,17 @@
 <?php
 	// Get all blogposts
 	$blogposts = $kirby->collection("blogposts");
-	
-	// Cut out the first one
-	$article = $blogposts->first();
-	$blogposts->remove($blogposts->first());
-	
+		
 	// Paginate the rest of the posts
-	$articles = $blogposts->paginate(12);
+	$articles = $blogposts->paginate(3);
 	$pagination = $articles->pagination();
 ?>
 <div class="page-layout home">
-	<?php if($pagination->page() == $pagination->firstPage()) : ?>
-		<article class="most-recent">
-			<a href="<?= $article->url() ?>">
-				<h2><?= $article->title() ?></h2>
-				<time datetime="<?= $page->date()->toDate('Y-MM-FF kk-mm-ss.SSSxxx') ?>"><?= renderPubdate($article, $kirby) ?></time>
-			</a>
-		</article>
-	<?php
-		endif;
-		
-		foreach($articles as $article) :
-	?>
+	<?php foreach($articles as $article) : ?>
 		<article>
-			<a href="<?= $article->url() ?>">
-				<h2><?= $article->title() ?></h2>
-				<time datetime="<?= $page->date()->toDate('Y-MM-FF kk-mm-ss.SSSxxx') ?>"><?= renderPubdate($article, $kirby) ?></time>
-			</a>
+			<a href="<?= $article->url() ?>"><h2><?= $article->title() ?></h2></a>
+			<time datetime="<?= $page->date()->toDate('Y-MM-FF kk-mm-ss.SSSxxx') ?>"><?= renderPubdate($article, $kirby) ?></time>
+			<?= $article->text()->kt() ?>
 		</article>
 	<?php endforeach; ?>
 	
