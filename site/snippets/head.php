6619e85af44e671425130dfd9faf9af5d47ef1b2
stairjoke <wenzel@wenzels.design>
2025-01-07 12:35:35 +0100

    corr—css config was faulty

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -2,9 +2,10 @@
   <title><?= $kirby->site()->title() ?></title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?= css(
-    'assets/css/style.css', '@auto'
-  ) ?>
+  <?= css([
+    'assets/css/style.css',
+    '@auto'
+  ]) ?>
   <link rel="alternate" type="application/rss+xml" 
     title="RSS Feed" 
     href="/feed.xml" />
