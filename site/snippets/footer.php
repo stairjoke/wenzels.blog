e320221d8ca0bb40039598b53499c77ecc52da4f
stairjoke <wenzel@wenzels.design>
2025-01-12 16:29:14 +0100

    Updated Footer copyright-date to use Kirby functions

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,4 +1,4 @@
 <footer>
   <p>I’m on <a rel="me" href="https://indieweb.social/@stairjoke">Mastodon</a> <button onclick="navigator.clipboard.writeText('@stairjoke@indieweb.social'); alert('Mastodon ID copied to clipboard!')">click to copy "@stairjoke@indieweb.social"</button> and <a href="https://posts.cv/wenzel">Posts.</a></p>
-  <p class="copy">© Copyright {{ page.date | friendlyDate }}, Wenzel Massag. German copyright law applies, redistribution with attribution highly encouraged.</p>
+  <p class="copy">© Copyright <?= ($page->date()->isEmpty()) ? $page->modified(option('date.format')) : $page->date()->toDate(option('date.format')) ; ?>, Wenzel Massag. German copyright law applies, redistribution with attribution highly encouraged.</p>
 </footer>
