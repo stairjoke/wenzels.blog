e2d3adfa4946c0afd63d14865c563d91de98b34a
stairjoke <wenzel@wenzels.design>
2025-01-12 15:41:41 +0100

    Updating Permalink URLs to use Kirby function

--- site/snippets/default.php
+++ site/snippets/default.php
@@ -1,5 +1,5 @@
 <main>
   <h1><?= $page->title() ?></h1>
   <?= $page->text()->kt() ?>
-  <p id="permalink"><a href="{{ page.url | safe }}">⌞w⌝</a></p>
+  <p id="permalink"><a href="<?= $page->permalink() ?>">⌞w⌝</a></p>
 </main>
