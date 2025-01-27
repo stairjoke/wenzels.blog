bb5035bab769f69b21dd71f125d6d80ace2b842b
stairjoke <wenzel@wenzels.design>
2025-01-27 13:23:59 +0100

    added date formats for HTML and RSS use

--- site/config/config.php
+++ site/config/config.php
@@ -1,8 +1,11 @@
 <?php
+  date_default_timezone_set('Europe/Berlin');
   return [
     'date' => [
       'handler' => 'intl',
-      'format' => 'MMMM dd, YYYY'
+      'format' => 'MMMM dd, YYYY HH:mm',
+      'HTML' => "yyyy-MM-dd'T'HH:mmXXX",
+      'RSS' => "EEE, dd MMM yyyy HH:mm:ss ZZZZ"
     ],
     'locale' => 'en_US.utf-8',
     'markdown' => [
