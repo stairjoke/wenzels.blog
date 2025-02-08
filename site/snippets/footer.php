b0c34303e4f4d4a96a7d3f43d33cae074015495f
stairjoke <wenzel@wenzels.design>
2025-02-08 13:31:42 +0100

    Removed permalink, added punctuation to link list.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,10 +1,13 @@
 <footer>
-  <p>Find me on <?php
-  foreach($site->links()->toStructure() as $link):
+  Find me on <?php
+  $links = $site->links()->toStructure();
+  $numberOfLinksLeftOver = $links->count();
+  foreach($links as $link):
+    $numberOfLinksLeftOver--;
     ?><a href="<?= $link->href() ?>" rel="<?php
     if($link->rel()->isNotEmpty()):
     foreach($link->rel() as $item) {
       echo($item . ' ');
-    } endif; ?>external" target="_blank"><?= $link->text() ?></a><?php endforeach; ?></p>
+    } endif; ?>external" target="_blank"><?= $link->text() ?></a><?php e($numberOfLinksLeftOver <= 0, '.', ', ') ?><?php endforeach; ?></p>
   <p class="copy">© Copyright <?= ($page->date()->isEmpty()) ? $page->modified(option('date.EN')) : $page->date()->toDate(option('date.EN')) ; ?> Germany, <?= $site->copyrightName() ?>. <?= $site->copyrightNotice()->kirbytextinline() ?></p>
 </footer>
