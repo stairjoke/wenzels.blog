d0d5910ae8561a7a10c28e0c5dd7d97085b2a62f
stairjoke <wenzel@wenzels.design>
2025-02-08 13:31:14 +0100

    changed defaultcontent to blocks, added permalink to page title

--- site/snippets/default.php
+++ site/snippets/default.php
@@ -1,5 +1,4 @@
 <main>
-  <h1><?= $page->title() ?></h1>
-  <?= $page->text()->kt() ?>
-  <p id="permalink"><a href="<?= $page->permalink() ?>">⌞w⌝</a></p>
+  <h1><a class="permalink" href="<?= $page->permalink() ?>">#</a><?= $page->title() ?></h1>
+  <?= $page->blocks()->toBlocks() ?>
 </main>
